627f70bf42336e678a69d67b67a67b7d4fb588cb
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-12 09:32:45 -0500

    feat: update sur-i43/s10/2 js

---
+++ prc-i43-2023-1/semana10/calculadora-feliz/js/index.js
@@ -0,0 +1,29 @@
+const happyForm = document.forms['happyForm'];
+const result = document.getElementById('result');
+
+happyForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let value = 0;
+  const number1 = +happyForm['number1'].value;
+  const operator = happyForm['operator'].value;
+  const number2 = +happyForm['number2'].value;
+
+  switch (operator) {
+    case '+':
+      value = number1 + number2;
+      break;
+    case '-':
+      value = number1 - number2;
+      break;
+    case '*':
+      value = number1 * number2;
+      break;
+    case '/':
+      value = number1 / number2;
+      break;
+    default:
+      break;
+  }
+
+  result.textContent = value;
+});
