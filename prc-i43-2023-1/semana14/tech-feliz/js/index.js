e13cfb07f64dbd40c5acbd26ec52fbb4549ad84c
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-26 09:41:58 -0500

    feat: update sur-i43/s14/1 js slider

--- prc-i43-2023-1/semana14/tech-feliz/js/index.js
+++ prc-i43-2023-1/semana14/tech-feliz/js/index.js
@@ -23,4 +23,22 @@ window.addEventListener('DOMContentLoaded', () => {
       linksMenu.classList.remove('header-nav__link-list--open');
     }
   });
+
+  const previewArrow = document.getElementById('previewArrow');
+  const nextArrow = document.getElementById('nextArrow');
+  const slides = document.querySelectorAll('.testimony__testimony');
+
+  const changePosition = (change) => {
+    const currentSlide = +document.querySelector('.testimony__testimony--show').dataset.id;
+    let newTestimony = currentSlide + change;
+
+    (newTestimony < 0) && (newTestimony = slides.length - 1);
+    (newTestimony > slides.length - 1) && (newTestimony = 0);
+
+    slides[currentSlide].classList.remove('testimony__testimony--show');
+    slides[newTestimony].classList.add('testimony__testimony--show');
+  };
+
+  previewArrow.addEventListener('click', () => changePosition(-1))
+  nextArrow.addEventListener('click', () => changePosition(1))
 });
