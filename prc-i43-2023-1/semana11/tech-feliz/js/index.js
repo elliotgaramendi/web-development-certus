90054fea74ce5538269242131d3a4736adff9bcb
elliotgaramendi <elliotgaramendi@gmail.com>
2023-06-26 08:53:43 -0500

    feat: update sur-i43/s11/1 js header

--- prc-i43-2023-1/semana11/tech-feliz/js/index.js
+++ prc-i43-2023-1/semana11/tech-feliz/js/index.js
@@ -1,15 +1,26 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
   const menuIcon = document.querySelector('.header-nav__menu-icon');
   const closeIcon = document.querySelector('.header-nav__close-icon');
   const linksMenu = document.querySelector('.header-nav__link-list');
 
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('header--scroll', scrollY > 0);
+  });
+
   menuIcon.addEventListener('click', () => {
-    linksMenu.classList.add('header-nav__link-list--open')
+    linksMenu.classList.add('header-nav__link-list--open');
   });
 
   closeIcon.addEventListener('click', () => {
-    linksMenu.classList.remove('header-nav__link-list--open')
+    linksMenu.classList.remove('header-nav__link-list--open');
+  });
+
+  linksMenu.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+      linksMenu.classList.remove('header-nav__link-list--open');
+    }
   });
 });
