46d91f81746e71e7c6a6ba80adaa9e2f6fc5e5db
elliotgaramendi <elliotgaramendi@gmail.com>
2023-07-03 07:31:55 -0500

    feat: update sur-i43/s14/1 js faq

--- prc-i43-2023-1/semana14/tech-feliz/js/index.js
+++ prc-i43-2023-1/semana14/tech-feliz/js/index.js
@@ -1,44 +1,62 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const header = document.querySelector('.header');
-  const menuIcon = document.querySelector('.header-nav__menu-icon');
-  const closeIcon = document.querySelector('.header-nav__close-icon');
-  const linksMenu = document.querySelector('.header-nav__link-list');
+  // // Inicio header
+  // const header = document.querySelector('.header');
+  // const menuIcon = document.querySelector('.header-nav__menu-icon');
+  // const closeIcon = document.querySelector('.header-nav__close-icon');
+  // const linksMenu = document.querySelector('.header-nav__link-list');
 
-  window.addEventListener('scroll', () => {
-    header.classList.toggle('header--scroll', scrollY > 0);
+  // window.addEventListener('scroll', () => {
+  //   header.classList.toggle('header--scroll', scrollY > 0);
+  // });
+
+  // menuIcon.addEventListener('click', () => {
+  //   linksMenu.classList.add('header-nav__link-list--open');
+  // });
+
+  // closeIcon.addEventListener('click', () => {
+  //   linksMenu.classList.remove('header-nav__link-list--open');
+  // });
+
+  // linksMenu.addEventListener('click', (e) => {
+  //   if (e.target.tagName === 'A') {
+  //     linksMenu.classList.remove('header-nav__link-list--open');
+  //   }
+  // });
+  // // Fin header
+
+  // // Inicio slider
+  // const previewArrow = document.getElementById('previewArrow');
+  // const nextArrow = document.getElementById('nextArrow');
+  // const slides = document.querySelectorAll('.testimony__testimony');
+
+  // const changePosition = (change) => {
+  //   const currentSlide = +document.querySelector('.testimony__testimony--show').dataset.id;
+  //   let newTestimony = currentSlide + change;
+
+  //   (newTestimony < 0) && (newTestimony = slides.length - 1);
+  //   (newTestimony > slides.length - 1) && (newTestimony = 0);
+
+  //   slides[currentSlide].classList.remove('testimony__testimony--show');
+  //   slides[newTestimony].classList.add('testimony__testimony--show');
+  // };
+
+  // previewArrow.addEventListener('click', () => changePosition(-1));
+  // nextArrow.addEventListener('click', () => changePosition(1));
+  // // Fin slider slider
+
+  // Inicio faq
+  const questionsQuestion = document.querySelectorAll('.questions__question');
+  questionsQuestion.forEach(element => {
+    element.addEventListener('click', () => {
+      let height = 0;
+      const answer = element.nextElementSibling;
+
+      (answer.clientHeight === 0) && (height = answer.scrollHeight);
+      element.children[1].classList.toggle('questions__arrow--rotate-180');
+      answer.classList.toggle('questions__answer--open');
+      answer.style.height = height + 'px';
+    });
   });
-
-  menuIcon.addEventListener('click', () => {
-    linksMenu.classList.add('header-nav__link-list--open');
-  });
-
-  closeIcon.addEventListener('click', () => {
-    linksMenu.classList.remove('header-nav__link-list--open');
-  });
-
-  linksMenu.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-      linksMenu.classList.remove('header-nav__link-list--open');
-    }
-  });
-
-  const previewArrow = document.getElementById('previewArrow');
-  const nextArrow = document.getElementById('nextArrow');
-  const slides = document.querySelectorAll('.testimony__testimony');
-
-  const changePosition = (change) => {
-    const currentSlide = +document.querySelector('.testimony__testimony--show').dataset.id;
-    let newTestimony = currentSlide + change;
-
-    (newTestimony < 0) && (newTestimony = slides.length - 1);
-    (newTestimony > slides.length - 1) && (newTestimony = 0);
-
-    slides[currentSlide].classList.remove('testimony__testimony--show');
-    slides[newTestimony].classList.add('testimony__testimony--show');
-  };
-
-  previewArrow.addEventListener('click', () => changePosition(-1))
-  nextArrow.addEventListener('click', () => changePosition(1))
 });
